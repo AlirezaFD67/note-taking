bc93f2872d5814f9fce98f5edeb74553a7a9a373
AlirezaFD67 <alirezafd.dev@gmail.com>
2025-03-28 13:29:33 +0330

    [86c2ubp4p] Setup: Add meta data configuration

---
+++ src/app/lib/utils/meta.ts
@@ -0,0 +1,44 @@
+export const metadata = {
+  title: "Note-Taking App | دفترچه یادداشت آنلاین",
+  description:
+    "یک اپلیکیشن یادداشت‌برداری تحت وب برای مدیریت و ذخیره یادداشت‌های روزانه. ساده، سریع و کاربرپسند!",
+  keywords: [
+    "دفترچه یادداشت",
+    "یادداشت برداری",
+    "Note-Taking App",
+    "Next.js Notes",
+    "React Notes",
+    "Task Management",
+  ],
+  authors: [{ name: "AlirezaFD67", url: "https://github.com/AlirezaFD67" }],
+  creator: "AlirezaFD67",
+  openGraph: {
+    title: "Note-Taking App | دفترچه یادداشت آنلاین",
+    description:
+      "یک اپلیکیشن یادداشت‌برداری تحت وب برای مدیریت و ذخیره یادداشت‌های روزانه. ساده، سریع و کاربرپسند!",
+    url: "https://your-deployed-url.com",
+    siteName: "Note-Taking App",
+    images: [
+      {
+        url: "https://your-deployed-url.com/og-image.jpg", // تغییر به آدرس تصویر واقعی
+        width: 1200,
+        height: 630,
+        alt: "دفترچه یادداشت دیجیتال",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    title: "Note-Taking App | دفترچه یادداشت آنلاین",
+    card: "summary_large_image",
+    site: "@your_twitter_handle", // در صورت داشتن حساب توییتر
+    creator: "@your_twitter_handle",
+    images: ["https://your-deployed-url.com/twitter-card.jpg"], // تصویر مناسب
+    description:
+      "یک اپلیکیشن یادداشت‌برداری تحت وب برای مدیریت و ذخیره یادداشت‌های روزانه. ساده، سریع و کاربرپسند!",
+  },
+  robots: "index, follow",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
